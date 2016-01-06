6bd7dd03ab721a55554ca2a00ad65c6f3116f8d9
Akshay Gangwar <akshaygangwar2009@gmail.com>
2016-01-06 11:37:33 +0530

    Added Music Manager
    Changelog:
    1. Music Manager added to auto load music for different levels
    2. Added a placeholder Win Screen (in the works)

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+	private AudioSource audioSource;
+
+	void Awake() {
+		DontDestroyOnLoad (gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	void OnLevelWasLoaded(int level){
+		audioSource.PlayOneShot (levelMusicChangeArray [level]);
+	}
+}
